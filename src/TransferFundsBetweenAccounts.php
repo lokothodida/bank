127839ffc3af0a8aaa7b84b8fa1a31a89fd1ad14
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2020-03-29 14:08:10 +0100

    Implement refund case when sender cannot be debited

--- src/TransferFundsBetweenAccounts.php
+++ src/TransferFundsBetweenAccounts.php
@@ -15,7 +15,27 @@ final class TransferFundsBetweenAccounts
 
     public function __invoke(string $senderAccountId, string $recipientAccountId, int $amount): void
     {
+        $this->transferFundsOut($senderAccountId, $amount);
+
+        try {
+            $this->transferFundsIn($recipientAccountId, $amount);
+        } catch (\Exception $e) {
+            $this->refundSender($senderAccountId, $amount);
+        }
+    }
+
+    private function transferFundsOut(string $senderAccountId, int $amount): void
+    {
         ($this->withdraw)($senderAccountId, $amount);
+    }
+
+    private function transferFundsIn(string $recipientAccountId, int $amount): void
+    {
         ($this->deposit)($recipientAccountId, $amount);
+    }
+
+    private function refundSender(string $senderAccountId, int $amount): void
+    {
+        ($this->deposit)($senderAccountId, $amount);
     }
 }
--- test/TransferringFundsTest.php
+++ test/TransferringFundsTest.php
@@ -45,4 +45,41 @@ final class TransferringFundsTest extends TestCase
             $accounts->get('recipient-account-id')
         );
     }
+
+    public function testWhenTheTransferInFailsTheSenderIsRefunded(): void
+    {
+        $now = new DateTimeImmutable();
+        $accounts = new InMemoryAccountRepository();
+        $accounts->set(
+            'sender-account-id',
+            Account::open('sender-account-id', 'c1', $now)
+                ->deposit(new Money(50), $now),
+            );
+        $accounts->set(
+            'recipient-account-id',
+            Account::open('recipient-account-id', 'c2', $now)
+                ->close($now)
+        );
+
+        $clock = new StaticClock($now);
+        $command = new TransferFundsBetweenAccounts(
+            new WithdrawFromAccount($accounts, $clock),
+            new DepositIntoAccount($accounts, $clock)
+        );
+        $command('sender-account-id', 'recipient-account-id', 25);
+
+        $this->assertEquals(
+            Account::open('sender-account-id', 'c1', $now)
+                ->deposit(new Money(50), $now)
+                ->withdraw(new Money(25), $now)
+                ->deposit(new Money(25), $now),
+            $accounts->get('sender-account-id')
+        );
+
+        $this->assertEquals(
+            Account::open('recipient-account-id', 'c2', $now)
+                ->close($now),
+            $accounts->get('recipient-account-id')
+        );
+    }
 }
